2a99b05becc700f7505f5d2c303bf7f66f848c97
Eric Snow <ericsnowcurrently@gmail.com>
2015-05-11 09:03:01 -0600

    Support tagging tests.

---
+++ tags.go
@@ -0,0 +1,103 @@
+// Copyright 2015 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package testing
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+	"testing"
+
+	gc "gopkg.in/check.v1"
+)
+
+// These are generally useful tags to use in tests.
+const (
+	TagBase       = "base"
+	TagSmall      = "small"
+	TagMedium     = "medium"
+	TagLarge      = "large"
+	TagSmoke      = "smoke"      // Fast sanity-check.
+	TagFunctional = "functional" // Does not use test doubles for low-level.
+	// TODO(ericsnow) Add other tags? For example:
+	//  - external: test uses external resources (e.g. filesystem)
+	//  - cloud: test interacts with a cloud provider's API
+	//  - vm: test runs in a local VM (e.g. kvm) for isolation
+)
+
+var defaultTags = []string{
+	TagBase,
+}
+
+var (
+	includedTags []string
+)
+
+func init() {
+	smoke := flag.Bool("smoke", false, "Run the basic set of fast tests.")
+	include := flag.String("include-tags", "", "Tagged tests to run.")
+	flag.Parse()
+
+	includedTags = strings.Split(*include, ",")
+	if *smoke {
+		includedTags = append(includedTags, TagSmoke)
+	}
+	if len(includedTags) == 0 {
+		includedTags = defaultTags
+	}
+
+	// TODO(ericsnow) support implied tags (e.g. VM -> Large)?
+}
+
+// CheckTag determines whether or not any of the given tags were passed
+// in at the commandline.
+func CheckTag(tags ...string) bool {
+	return MatchTag(tags...) != ""
+}
+
+// MatchTag returns the first provided tag that matches the ones passed
+// in at the commandline.
+func MatchTag(tags ...string) string {
+	for _, tag := range tags {
+		for _, includedTag := range includedTags {
+			if tag == includedTag {
+				return tag
+			}
+		}
+	}
+	return ""
+}
+
+// RegisterPackageTagged registers the package for testing if any of the
+// given tags were passed in at the commandline.
+func RegisterPackageTagged(t *testing.T, tags ...string) {
+	if CheckTag(tags...) {
+		gc.TestingT(t)
+	}
+}
+
+// SuiteTagged registers the suite with the test runner if any of the
+// given tags were passed in at the commandline.
+func SuiteTagged(suite interface{}, tags ...string) {
+	if CheckTag(tags...) {
+		gc.Suite(suite)
+	}
+}
+
+// RequireTag causes a test or suite to skip if none of the given tags
+// were passed in at the commandline.
+func RequireTag(c *gc.C, tags ...string) {
+	if !CheckTag(tags...) {
+		c.Skip(fmt.Sprintf("skipping due to no matching tags (%v)", tags))
+	}
+}
+
+// SkipTag causes a test or suite to skip if any of the given tags were
+// passed in at the commandline.
+func SkipTag(c *gc.C, tags ...string) {
+	matched := MatchTag(tags...)
+	if matched != "" {
+		c.Skip(fmt.Sprintf("skipping due to %q tag", matched))
+	}
+}
